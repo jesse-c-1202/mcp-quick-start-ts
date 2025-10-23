3b6d80862e237b78fc08303c7381d5ff9fffe94d
Jesse <caoyongjian.jesse@gmail.com>
2025-10-23 10:46:15 +0800

    Update chatWithMCP.js

--- srv/chatWithMCP.js
+++ srv/chatWithMCP.js
@@ -9,7 +9,7 @@ async function main() {
     const mcpClient = new MCPClient_1.MCPClient();
     try {
         // await mcpClient.connectToServer(process.argv[2]);
-        await mcpClient.connectToServer("/Users/I540371/CAP/Example/mcp-quick-start-ts/srv/server.js");
+        await mcpClient.connectToServer("XXX/mcp-quick-start-ts/srv/server.js");
         await mcpClient.chatLoop();
     }
     finally {
